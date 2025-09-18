9e511b6f0d11ff62c392e83e2df14e1bff026453
Kezara Lakshan <87107996+Kezara666@users.noreply.github.com>
2025-09-18 16:30:02 +0530

    point to backend

--- pos_master_web/src/environments/environment.prod.ts
+++ pos_master_web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //backendUrl: 'https://pos-kesara.nimbuscode.online',
+  backendUrl: 'https://pos-kesara.nimbuscode.online',
   localBillUrl: 'http://localhost:5000', // Local backend URL for printing bills
-  backendUrl: 'http://localhost:4000'
+  //backendUrl: 'http://localhost:4000'
 };
