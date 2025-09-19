df685e9e69d66c3065e464285261b455829c6c3c
Kezara Lakshan <87107996+Kezara666@users.noreply.github.com>
2025-09-19 11:34:29 +0530

    https://pos-kesara-dress.nimbuscode.online/api

--- pos_master_web/src/environments/environment.prod.ts
+++ pos_master_web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://pos-kesara.nimbuscode.online',
+  backendUrl: 'https://pos-kesara-dress.nimbuscode.online',
   localBillUrl: 'http://localhost:5000', // Local backend URL for printing bills
   //backendUrl: 'http://localhost:4000'
 };
