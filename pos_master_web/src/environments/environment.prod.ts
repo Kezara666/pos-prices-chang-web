efaea80c2b4fd0937fdcb1e805938dd18bede2f7
Kezara Lakshan <87107996+Kezara666@users.noreply.github.com>
2025-09-19 16:04:36 +0530

    https://sk-fashion-mocha.vercel.app/login

--- pos_master_web/src/environments/environment.prod.ts
+++ pos_master_web/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://pos-kesara-dress.nimbuscode.online',
+  backendUrl: 'https://https://sk-fashion-mocha.vercel.app',
+  
   localBillUrl: 'http://localhost:5000', // Local backend URL for printing bills
   //backendUrl: 'http://localhost:4000'
 };
