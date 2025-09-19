a2dc5f4f0486ef60b0f53cc1298934d59634604d
Kezara Lakshan <87107996+Kezara666@users.noreply.github.com>
2025-09-19 16:05:18 +0530

    fashion-sk.harshana.store

--- pos_master_web/src/environments/environment.prod.ts
+++ pos_master_web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: 'https://https://sk-fashion-mocha.vercel.app',
+  backendUrl: 'https://fashion-sk.harshana.store',
   
   localBillUrl: 'http://localhost:5000', // Local backend URL for printing bills
   //backendUrl: 'http://localhost:4000'
